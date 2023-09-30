aa033227e720af5e6cd9cc3ffbbe8990088f0a02
Nigel Tay <82434359+nigel-tay@users.noreply.github.com>
2023-09-30 22:19:42 +0800

    move client to own directory, include backend, combine gitignore

---
+++ WhatToDoInSingapore_backend/src/main/java/project/WhatToDoInSingapore/WhatToDoInSingaporeApplication.java
@@ -0,0 +1,13 @@
+package project.WhatToDoInSingapore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatToDoInSingaporeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatToDoInSingaporeApplication.class, args);
+	}
+
+}
---
+++ WhatToDoInSingapore_backend/src/test/java/project/WhatToDoInSingapore/WhatToDoInSingaporeApplicationTests.java
@@ -0,0 +1,13 @@
+package project.WhatToDoInSingapore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatToDoInSingaporeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
