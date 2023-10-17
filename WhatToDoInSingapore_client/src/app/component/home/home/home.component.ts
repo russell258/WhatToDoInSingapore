82a0bb03984611a73c4e2a9ff72b63bd6ed9676f
Nigel Tay <82434359+nigel-tay@users.noreply.github.com>
2023-10-17 21:56:01 +0800

    add switch case and linkArray

--- WhatToDoInSingapore_client/src/app/component/home/home/home.component.ts
+++ WhatToDoInSingapore_client/src/app/component/home/home/home.component.ts
@@ -7,4 +7,56 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  linkArray: string[] = [
+    "accomodation",
+    "attractions",
+    "nightlife",
+    "cruises",
+    "events",
+    "fb",
+    "shoppingmall",
+    "tours",
+    "venues", 
+    "trails"
+  ]
+
+  tagText: string = "";
+
+  constructor(){}
+
+  determineTextFromLink(link: string) {
+    switch (link) {
+      case "accomodation":
+        this.tagText = "Accomodation";
+        break;
+      case "attractions":
+        this.tagText = "Attractions";
+        break;
+      case "nightlife":
+        this.tagText = "Bars & Clubs";
+        break;
+      case "cruises":
+        this.tagText = "Cruises";
+        break;
+      case "events":
+        this.tagText = "Events";
+        break;
+      case "fb":
+        this.tagText = "Food & Beverages";
+        break;
+      case "shoppingmall":
+        this.tagText = "Malls & Shops";
+        break;
+      case "tours":
+        this.tagText = "Tours";
+        break;
+      case "venues":
+        this.tagText = "Venues";
+        break;
+      case "trails":
+        this.tagText = "Walking Trails";
+        break;
+    }
+    return this.tagText;
+  }
 }
